7a486637f60247116a0545806555a75247e1d6ca
Arzu1996 <yagut.badalova@gmail.com>
2019-12-07 12:47:50 +0400

    --initial

---
+++ src/main/java/app/entity/User.java
@@ -0,0 +1,14 @@
+package app.entity;
+
+public class User {
+    private String person;
+
+    public User(String person) {
+        this.person = person;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+}
